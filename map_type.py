08f7032ad90623982d829bcf2f18d6a9bef2136b
Jacob <thelogicalweapon@yahoo.com>
2018-01-10 21:03:33 -0800

    Python?

--- map_type.py
+++ map_type.py
@@ -2,15 +2,19 @@ import battlecode as bc
 #Find all deposits on earth
 print("map_type works")
 deposit_locations_earth = []
-map_width = bc.PlanetMap.width
+
+pm = bc.PlanetMap()
+
+map_width = pm.width
+
 for x_coordinate in range(map_width):
-    for y_coordinate in range(bc.height):
+    for y_coordinate in range(pm.height):
         coord = [x_coordinate,y_coordinate]
         if bc.has_deposit(coord) == True:
-            bc.deposit_locations_earth.append(coord)
+            deposit_locations_earth.append(coord)
 
 
 #sum of total resources
-total_earth_deposit = 0
-for i in deposit_locations_earth:
-        total_earth_deposit += bc.initial_karbonite_at(i[0],i[1])
+#total_earth_deposit = 0
+#for i in deposit_locations_earth:
+#        total_earth_deposit += bc.initial_karbonite_at(i[0],i[1])
