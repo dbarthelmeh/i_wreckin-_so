8903306feab3b868bd7f94e959b58dbfcc5c0db0
dbarthelmeh <dcbarthelmeh@gmail.com>
2018-01-13 08:31:23 -0700

    map now blank

---
+++ map_type.py
@@ -0,0 +1,15 @@
+#Find all deposits on earth
+print("map_type works")
+#deposit_locations_earth = []
+#print(bc.PlanetMap.Planet)
+#for x_coordinate in range(bc.GameController().starting_map().width):
+#    for y_coordinate in range(bc.GameController().starting_map().height):
+#        coord = [x_coordinate,y_coordinate]
+#        if bc.GameController.initial_karbonite_at(coord) > 0:
+#            deposit_locations_earth.append(coord)
+
+
+#sum of total resources
+#total_earth_deposit = 0
+#for i in deposit_locations_earth:
+#        total_earth_deposit += gc.initial_karbonite_at(i[0],i[1])
